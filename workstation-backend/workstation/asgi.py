b51298a389e8131843849673f09095f124f8e3dd
Marcella (Cindy) Prasetio <m.cindy.prasetio@gmail.com>
2022-01-30 14:19:51 -0800

    Setup up initial backend project

---
+++ workstation-backend/workstation/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for workstation project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workstation.settings')
+
+application = get_asgi_application()
