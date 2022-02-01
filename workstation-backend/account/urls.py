59d3a8e52764940c4065a5c90324137aeed21e69
Marcella (Cindy) Prasetio <m.cindy.prasetio@gmail.com>
2022-01-31 23:51:36 -0800

    Add test to account registration

--- workstation-backend/account/urls.py
+++ workstation-backend/account/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import RegisterView
 
 urlpatterns = [
-    path("register", RegisterView.as_view()),
+    path("register", RegisterView.as_view(), name="account-register"),
 ]
---
+++ workstation-backend/tests/account/test_views.py
@@ -0,0 +1,23 @@
+import pytest
+
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_register(client):
+    url = reverse("account-register")
+    response = client.post(
+        url,
+        data={
+            "username": "test_user",
+            "email": "test@test.com",
+            "password": "testpassword",
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "username": "test_user",
+        "message": (
+            "User Created Successfully. Perform Login to get your token."
+        ),
+    }
