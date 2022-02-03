ea6678d4255e2ad6016c3567be6f28f5c5b4301a
Randy Lirano <randylirano@Randys-MacBook-Pro.local>
2022-02-02 17:34:28 -0800

    Remove account model to use Django pre-built

--- workstation-backend/account/models.py
+++ workstation-backend/account/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 
-class User(models.Model):
-    email = models.EmailField(max_length=254)
-    username = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
+# Create your models here.
