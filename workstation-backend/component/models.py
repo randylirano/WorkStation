21aeb3c5d19f7b097c0959206d4674ae16c4637f
Randy Lirano <randylirano@Randys-MacBook-Pro.local>
2022-02-04 22:13:41 -0800

    Created all DB tables for workspace and components

---
+++ workstation-backend/component/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from workspace import models as workspace_models
+
+# Create your models here.
+class BaseComponent(models.Model):
+    workspace_id = models.ForeignKey(workspace_models.Workspace, on_delete=models.CASCADE)
+    x = models.DecimalField(max_digits=100, decimal_places=2)
+    y = models.DecimalField(max_digits=100, decimal_places=2)
+    width = models.DecimalField(max_digits=100, decimal_places=2)
+    height = models.DecimalField(max_digits=100, decimal_places=2)
+
+    class Meta:
+        abstract = True
+
+
+# Checklist component
+class Checklist(BaseComponent):
+    title = models.CharField(max_length=200)
+    collapsed = models.BooleanField()
+    color = models.CharField(max_length=10, default="#baaf13")
+
+
+class ChecklistItem(models.Model):
+    checklist_id = models.ForeignKey(Checklist, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    is_checked = models.BooleanField()
+
+
+class PostIt(BaseComponent):
+    content = models.CharField(max_length=200)
+    collapsed = models.BooleanField()
+    color = models.CharField(max_length=10, default="#baaf13")
+
+
+class Image(BaseComponent):
+    url = models.URLField()
--- workstation-backend/workspace/models.py
+++ workstation-backend/workspace/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Workspace(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+
+class Background(models.Model):
+    workspace_id = models.ForeignKey(Workspace, on_delete=models.CASCADE)
+    image_url = models.URLField()
